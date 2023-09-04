4d3c84f48a6eb393b99a5c77330f318a9ad7c757
pabcrudel <pabcrudel@gmail.com>
2023-09-04 18:05:47 +0200

    Added email as an attribute

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,4 +1,8 @@
-import { CognitoIdentityProviderClient, SignUpCommand, SignUpCommandInput } from '@aws-sdk/client-cognito-identity-provider';
+import {
+    CognitoIdentityProviderClient,
+    SignUpCommand,
+    SignUpCommandInput
+} from '@aws-sdk/client-cognito-identity-provider';
 import { RequestFunction } from "./types";
 import { ApiErrorResponse, ApiSuccessResponse, BadRequestError, ServerError } from "./utils";
 
@@ -13,13 +17,16 @@ const cognito = new CognitoIdentityProviderClient({ region: userPoolRegion });
 export const signUp: RequestFunction = async (event) => {
     try {
         if (event.body === null) throw new BadRequestError("Empty request body");
-        
-        const { password, username } = JSON.parse(event.body);
+
+        const { password, username, email } = JSON.parse(event.body);
 
         const input: SignUpCommandInput = {
             ClientId: clientId,
             Password: password,
             Username: username,
+            UserAttributes: [
+                { Name: "email", Value: email },
+            ]
         };
 
         const response = await cognito.send(new SignUpCommand(input));
