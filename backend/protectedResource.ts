2ad6bc60aa829d6d00dd5facfc96390ff190ffa4
pabcrudel <pabcrudel@gmail.com>
2023-09-05 12:25:18 +0200

    Deleted unused files

--- backend/protectedResource.ts
+++
@@ -1,6 +0,0 @@
-import { RequestFunction } from "./types"; 
-import { ApiSuccessResponse } from "./utils";
-
-export const handler: RequestFunction = async (event) => {
-    return new ApiSuccessResponse('Hello protected lambda');
-};
--- oauth2/types.d.ts
+++
@@ -1,9 +0,0 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-
-export interface RequestFunction {
-    (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult | any>
-}
-
-export interface GetAccessTokenFromCookies {
-    (cookies: string | undefined): string | undefined
-}
