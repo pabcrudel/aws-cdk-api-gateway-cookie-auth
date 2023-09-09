9e9ddfe7d9829279ecd20b287d93180ef90cb79a
pabcrudel <pabcrudel@gmail.com>
2023-09-09 18:21:20 +0200

    Created a Workflow to automate deployment and destroying

--- backend/bin/app.ts
+++ backend/bin/app.ts
@@ -4,6 +4,12 @@ import * as cdk from 'aws-cdk-lib';
 import { CognitoSecuredRestApi } from '../lib/cognito-secured-rest-api';
 import { WebsiteDeployment } from '../lib/website-deployment';
 
+let projectName = 'CognitoSecured-';
+
+const tag = process.env.BRANCH_NAME;
+
+if (tag !== undefined) projectName = `${tag}-${projectName}`;
+
 const app = new cdk.App();
-new CognitoSecuredRestApi(app, 'CognitoSecuredRestApi');
-new WebsiteDeployment(app, 'CognitoSecuredWebsiteDeployment');
+new CognitoSecuredRestApi(app, `${projectName}RestApi`);
+new WebsiteDeployment(app, `${projectName}WebsiteDeployment`);
